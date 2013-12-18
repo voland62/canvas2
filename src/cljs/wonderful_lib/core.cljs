3c1df3bdf53e02d8e0a00a7da7cce9309de71e77
Andrii Zhukov <azhukov@luxoft.com>
2013-12-18 18:15:28 +0200

    kind of loop added. Start playing with custom types in cljs

--- src/cljs/wonderful_lib/core.cljs
+++ src/cljs/wonderful_lib/core.cljs
@@ -4,7 +4,8 @@
 ;;; if you can add files containing macros and compile-time only
 ;;; functions in the :source-paths setting of the :builds, it is
 ;;; strongly suggested to add them to the leiningen :source-paths.
-(ns wonderful-lib.core)
+(ns wonderful-lib.core
+  (:require wonderful-lib.types))
 
 (defn foo [greeting]
   (if greeting
@@ -19,16 +20,44 @@
 (def canvas (.getElementById js/document "myCanvas"))
 (def context (.getContext canvas "2d"))
 
-(set! (.-fillStyle context) "#ffffaa")
-(.fillRect context 0 0 500 300)
+;;(set! (.-fillStyle context) "#ffffaa")
+;;(.fillRect context 0 0 500 300)
 
 
-(defn tick
-  [t] (println "tick" t))
+
 
 (js/requestAnimationFrame tick)
 
-(set! (.-fillStyle context) "#ff0000")
-(.beginPath context)
-(.arc context 100 100 15 0 (* 2 Math.PI))
-(.fill context)
+(defn drawCircle [col]
+  (do
+    (set! (.-fillStyle context) col)
+    (.beginPath context)
+    (.arc context 100 100 15 0 (* 2 Math.PI))
+    (.fill context)))
+
+;; проверить ссылки
+
+(def looping? true)
+
+
+(defn main-loop []
+  (println "aha"))
+
+
+(defn tick
+  [t] (do
+        ;;(println "tick" t)
+        (main-loop)
+        (if looping?
+            (js/requestAnimationFrame tick))))
+
+(defn start-loop []
+  (do
+    (set! looping? true)
+    (tick nil)))
+
+(defn stop-loop []
+  (set! looping? false))
+
+;;(start-loop)
+;;(stop-loop)
---
+++ src/cljs/wonderful_lib/types.cljs
@@ -0,0 +1,1 @@
+(ns wonderful-lib.types)
